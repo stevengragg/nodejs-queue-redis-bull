e31888456b3ab6d98c0d52b143d5c28e05e75b6e
sgragg5619 <sgragg5619@gmail.com>
2022-02-23 05:03:08 +0800

    Pretty

--- src/processes/email.process.ts
+++ src/processes/email.process.ts
@@ -2,30 +2,29 @@ import { Job } from "bull";
 import nodemailer from "nodemailer";
 
 const emailProcess = async (job: Job) => {
+  const testAccount = await nodemailer.createTestAccount();
+  console.log(job.data);
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
 
-    let testAccount = await nodemailer.createTestAccount();
+  // send mail with defined transport object
+  const info = await transporter.sendMail(job.data);
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail(job.data);
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    return nodemailer.getTestMessageUrl(info);
+  return nodemailer.getTestMessageUrl(info);
 };
 
 export default emailProcess;
--- src/queues/email.queue.ts
+++ src/queues/email.queue.ts
@@ -1,25 +1,34 @@
-import Bull from 'bull';
-import emailProcess from '../processes/email.process';
-import {setQueues, BullAdapter} from 'bull-board';
+import Bull from "bull";
+import emailProcess from "../processes/email.process";
+import { setQueues, BullAdapter } from "bull-board";
 
 // https://optimalbits.github.io/bull
 
-const emailQueue = new Bull('email', {
-    redis: process.env.REDIS_URL
+const emailQueue = new Bull("email", {
+  redis: { port: 6379, host: "127.0.0.1", password: "" }
 });
 
-setQueues([
-    new BullAdapter(emailQueue)
-]);
+setQueues([new BullAdapter(emailQueue)]);
 
-emailQueue.process(emailProcess);
+emailQueue.process(100, emailProcess);
 
 const sendNewEmail = (data: any) => {
-    emailQueue.add(data, {
-        attempts: 5
-    });
+  console.log("Send new email queue");
+  emailQueue.add(data, {
+    attempts: 5
+  });
 };
 
-export {
-    sendNewEmail
-}
+emailQueue.on("completed", (job: any, result: any) => {
+  // Job completed with output result!
+  console.log("job: output", { job });
+  console.log("result: output", { result });
+  console.log("Email task completed");
+});
+
+emailQueue.on("failed", (error: any) => {
+  // An error occured.
+  console.log("Error occured", { errorMessage: error.failedReason });
+});
+
+export { sendNewEmail };
